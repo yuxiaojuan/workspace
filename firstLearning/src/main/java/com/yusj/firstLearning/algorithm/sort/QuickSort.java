6b762149c06a538d535fc04634672db7c60e3baa
yuxiaojuan <347239140@qq.com>
2021-06-09 22:30:53 +0800

    完成quick sort

--- firstLearning/src/main/java/com/yusj/firstLearning/algorithm/sort/QuickSort.java
+++ firstLearning/src/main/java/com/yusj/firstLearning/algorithm/sort/QuickSort.java
@@ -5,33 +5,35 @@ import java.util.Arrays;
 public class QuickSort {
 
     public static void main(String[] args) {
-        int[] list = {2,4,6,3};
+        System.out.println("Quick Sort.");
+        int[] list = {2,4,6,3,1,5,8,23,12,7,9};
         sort(list,0,list.length - 1);
         System.out.println(Arrays.toString(list));
     }
 
     public static void sort(int[] list,int start,int end){
         if (start >= end) return;
-        int j = compare(list,start,end);
+        int j = partition(list,start,end);
         sort(list,start,j - 1);
         sort(list,j+1,end);
     }
 
-    public static int compare(int[] list,int start,int end){
+    public static int partition(int[] list,int start,int end){
         int result = list[start];
-        int i = start - 1;
+        int i = start;
         int j = end + 1;
-        while(i >= j){
-            while (list[++i]<result){
-                 if (i >= end) break;
+        while(i < j){
+            while (list[++i] < result){
+                 if (i == end) break;
             }
-            while (list[--j]>result){
-                if (j <= start) break;
+            while (list[--j] > result){
+                if (j == start) break;
             }
             if(i < j){
                 swap(list,i,j);
             }
         }
+        swap(list,start,j);
         return j;
     }
 
